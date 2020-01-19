cb744ccea65448d5c8e37de3facd2ce7f575f372
chefmohima <38052670+chefmohima@users.noreply.github.com>
2020-01-19 16:54:59 +0530

    Create remove_duplicates_from_list.py

---
+++ remove_duplicates_from_list.py
@@ -0,0 +1,34 @@
+"""
+Template - Remove duplicates from a list while preserving the order of items
+"""
+
+myList = [1, 2, 3, 1, 2, 5, 6, 7, 8]
+cleanlist = []
+[cleanlist.append(x) for x in myList if x not in cleanlist]
+
+
+def remove_duplicates(items):
+    """
+    Given a list, return a list with duplicate items removed
+    and the remaining items in the same order
+    """
+    result = []
+    for item in items:
+        if item not in result:
+            result.append(item) 
+    return result
+
+
+
+# Test code
+print(remove_duplicates([]))
+print(remove_duplicates([1, 2, 3, 4]))
+print(remove_duplicates([1, 2, 2, 3, 3, 3, 4, 5, 6, 6]))
+print(remove_duplicates(["cat", "dog", "cat", "pig", "cow", "cat", "pig", "pug"]))
+
+
+# Output
+#[]
+#[1, 2, 3, 4]
+#[1, 2, 3, 4, 5, 6]
+#['cat', 'dog', 'pig', 'cow', 'pug']
